a7a7048b17b5ce87e1fc36069c2e0c527b985156
JaynAp1 <jerxonmjair@gmail.com>
2025-03-09 16:57:23 -0600

    :sparkles: update music

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-document.getElementById("AudioMusic").volume = 0.03;
+document.addEventListener("DOMContentLoaded", () => {
+    const audio = document.getElementById("AudioMusic");
+    audio.volume = 0.03;
 
+    document.body.addEventListener("click", () => {
+        audio.muted = false;
+        audio.play();
+    }, { once: true });
+});
