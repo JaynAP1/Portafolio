eb46bb319351db5565ef6c70d0903cf1a3e59c47
JaynAp1 <jerxonmjair@gmail.com>
2025-03-09 22:06:27 -0600

    :sparkles: update portafolio

--- js/script.js
+++ js/script.js
@@ -7,3 +7,50 @@ document.addEventListener("DOMContentLoaded", () => {
         audio.play();
     }, { once: true });
 });
+
+
+
+document.querySelector(".Change").addEventListener("click",()=>{
+    cambiarCSS()
+    cambiarCancion()
+})
+
+function cambiarCSS() {
+    let link = document.getElementById("Estilos");
+    if (link.getAttribute("href") === "css/indexCss.css") {
+        link.setAttribute("href", "css/indexCss2.css");
+    } else {
+        link.setAttribute("href", "css/indexCss.css");
+    }
+}
+function cambiarCancion(){
+    let audio = document.getElementById("AudioMusic");
+    if (audio.getAttribute("src") === "src/FlyMeto theMoon.mp3") {
+        audio.setAttribute("src", "src/Littleroot.mp3");
+        audio.play();
+    } else {
+        audio.setAttribute("src", "src/FlyMeto theMoon.mp3");
+        audio.play();
+    }
+}
+
+
+
+
+
+
+
+
+
+//Control Volumen
+document.getElementById("Mute").addEventListener("click",() =>{
+    const audio = document.getElementById("AudioMusic");
+    const iconMute = document.getElementById("Mute");
+    if(audio.volume === 0){
+        audio.volume = 0.03;
+        iconMute.className = "bx bxs-volume-full"
+    }else{
+        audio.volume = 0;
+        iconMute.className = "bx bxs-volume-mute"
+    }
+})
